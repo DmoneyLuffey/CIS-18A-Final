d8b31c8a28800b50e640a6a1ed5a80238af83a04
Damani Ward <dmoneyluffey@gmail.com>
2020-05-24 00:04:47 +0000

    Added another print test

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("This is a test");
+    System.out.printl("This is a second test");
   }
 }
