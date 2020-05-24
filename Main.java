9771e991fe7f2c6ff3944575b6c51d5ce5101060
Damani Ward <dmoneyluffey@gmail.com>
2020-05-24 00:24:26 +0000

    Fourth Test

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("This is a test");
-    System.out.printl("This is a second test");
+    System.out.println("This is a second test");
+    System.out.println("This is a fourth test");
   }
 }
