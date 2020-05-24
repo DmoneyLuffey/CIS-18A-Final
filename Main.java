54942a12780227131e888c6edc586bdd679f326d
Gianni Rahman <giannir08@gmail.com>
2020-05-24 01:19:53 +0000

    added new line

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ class Main {
     System.out.println("Hello world!");
     System.out.println("This is a test");
     System.out.println("This is a second test");
+<<<<<<< HEAD
     System.out.println("This is a third test");
+=======
+    System.out.println("This is a fourth test");
+>>>>>>> origin/master
+System.out.println("Hello");
   }
 }
