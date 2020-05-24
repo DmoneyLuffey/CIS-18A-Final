82de2f9fcd540f8a4803ededc2b7d538801f6b96
Damani Ward <dmoneyluffey@gmail.com>
2020-05-24 01:49:46 +0000

    Hello 3

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("This is a test");
+<<<<<<< HEAD
     System.out.println("This is a second test");
 <<<<<<< HEAD
     System.out.println("This is a third test");
@@ -10,5 +11,12 @@ class Main {
 >>>>>>> origin/master
 System.out.println("Hello");
 system.out.println("Hi there!")
+=======
+System.out.println("Hello!");
+System.out.println("Hello!2");
+System.out.println("Hello 3");
+
+>>>>>>> origin/master
   }
+
 }
