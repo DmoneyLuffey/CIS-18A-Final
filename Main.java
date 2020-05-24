ddc4d64efe377b97087de298c33dc8518e935c0d
Damani Ward <dmoneyluffey@gmail.com>
2020-05-24 01:59:30 +0000

    Reset

--- Main.java
+++ Main.java
@@ -1,22 +1,5 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    System.out.println("This is a test");
-<<<<<<< HEAD
-    System.out.println("This is a second test");
-<<<<<<< HEAD
-    System.out.println("This is a third test");
-=======
-    System.out.println("This is a fourth test");
->>>>>>> origin/master
-System.out.println("Hello");
-system.out.println("Hi there!")
-=======
-System.out.println("Hello!");
-System.out.println("Hello!2");
-System.out.println("Hello! 3");
-
->>>>>>> origin/master
   }
-
 }
